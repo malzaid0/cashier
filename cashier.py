8025e9342ea64d4f4a500d84f5e06b0f98407b0d
Mohammed Alzaid <m.al-zaid@live.com>
2020-08-17 10:35:25 +0300

    completed the code

--- cashier.py
+++ cashier.py
@@ -1,5 +1,30 @@
 def main():
-	# write code here
+    # write code here
+
+    receipt = []
+    total = 0
+    entered = input("Item (enter \"done\" when finished): ")
+
+    while entered != "done":
+        item_details = {}
+        item_details["name"] = entered
+        item_details["price"] = float(input("Price: "))
+        item_details["quantity"] = int(input("Quantity: "))
+        receipt.append(item_details)
+        entered = input("Item (enter \"done\" when finished): ")
+
+    print("\n-------------------")
+    print("receipt")
+    print("-------------------")
+
+    for item in receipt:
+        item_total = item["quantity"] * item["price"]
+        total += item_total
+        print(item["quantity"], item["name"], str("{:.3f}".format(item_total)) + "KD")
+
+    print("-------------------")
+    print("Total Price: " + str("{:.3f}".format(total)) + "KD")
+
 
 if __name__ == '__main__':
-	main()
+    main()
